9bbc91b0453ba51ff8f7ca30bcaad41ea92582c3
sielus <kubasie1997@gmail.com>
2020-08-29 23:55:13 +0200

    1.4b System płatności (wstępny)

--- app/src/main/java/com/devdreams/energii/koszt/MainActivity.java
+++ app/src/main/java/com/devdreams/energii/koszt/MainActivity.java
@@ -5,11 +5,22 @@ import android.content.ClipboardManager;
 import android.content.Intent;
 import android.content.SharedPreferences;
 import android.os.Bundle;
+import android.util.Log;
 import android.view.Menu;
 import android.view.MenuItem;
 import android.view.View;
 import android.widget.TextView;
 import android.widget.Toast;
+
+import com.android.billingclient.api.BillingClient;
+import com.android.billingclient.api.BillingClientStateListener;
+import com.android.billingclient.api.BillingFlowParams;
+import com.android.billingclient.api.BillingResult;
+import com.android.billingclient.api.Purchase;
+import com.android.billingclient.api.PurchasesUpdatedListener;
+import com.android.billingclient.api.SkuDetails;
+import com.android.billingclient.api.SkuDetailsParams;
+import com.android.billingclient.api.SkuDetailsResponseListener;
 import com.blogspot.atifsoftwares.animatoolib.Animatoo;
 import com.devdreams.energii.koszt.ui.rooms.RoomListFragment;
 import com.devdreams.energii.koszt.ui.settings.SettingActivity;
@@ -17,6 +28,8 @@ import com.google.android.gms.ads.MobileAds;
 import com.google.android.gms.ads.initialization.InitializationStatus;
 import com.google.android.gms.ads.initialization.OnInitializationCompleteListener;
 import com.google.android.material.navigation.NavigationView;
+
+
 import androidx.navigation.NavController;
 import androidx.navigation.Navigation;
 import androidx.navigation.ui.AppBarConfiguration;
@@ -24,13 +37,19 @@ import androidx.navigation.ui.NavigationUI;
 import androidx.drawerlayout.widget.DrawerLayout;
 import androidx.appcompat.app.AppCompatActivity;
 import androidx.appcompat.widget.Toolbar;
+
+
+import java.util.ArrayList;
+import java.util.List;
 import java.util.Objects;
 
-public class MainActivity extends AppCompatActivity {
+public class MainActivity extends AppCompatActivity{
     public View view;
     private AppBarConfiguration mAppBarConfiguration;
     public static Toolbar toolbar;
-    public static boolean firstStart;
+    BillingClient billingClient;
+
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
@@ -56,6 +75,49 @@ public class MainActivity extends AppCompatActivity {
             }
         });
 
+
+        final PurchasesUpdatedListener purchaseUpdateListener = new PurchasesUpdatedListener() {
+            @Override
+            public void onPurchasesUpdated(BillingResult billingResult, List<Purchase> purchases) {
+                // To be implemented in a later section.
+
+                if(billingResult.getResponseCode() == BillingClient.BillingResponseCode.OK  && purchases!=null ){
+                    Toast.makeText(MainActivity.this,"OK",Toast.LENGTH_SHORT).show();
+                }else if(billingResult.getResponseCode() == BillingClient.BillingResponseCode.USER_CANCELED){
+                    Toast.makeText(MainActivity.this,"USER_CANCELED",Toast.LENGTH_SHORT).show();
+                }else if(billingResult.getResponseCode() == BillingClient.BillingResponseCode.ITEM_ALREADY_OWNED){
+                    Toast.makeText(MainActivity.this,"ITEM_ALREADY_OWNED xx",Toast.LENGTH_SHORT).show();
+                }else if(billingResult.getResponseCode() == BillingClient.BillingResponseCode.BILLING_UNAVAILABLE){
+                    Toast.makeText(MainActivity.this,"BILLING_UNAVAILABLE",Toast.LENGTH_SHORT).show();
+                }
+            }
+        };
+
+        billingClient = BillingClient.newBuilder(this)
+                .setListener(purchaseUpdateListener)
+                .enablePendingPurchases()
+                .build();
+
+        billingClient.startConnection(new BillingClientStateListener() {
+            @Override
+            public void onBillingSetupFinished(BillingResult billingResult) {
+                if (billingResult.getResponseCode() ==  BillingClient.BillingResponseCode.OK) {
+                    Toast.makeText(MainActivity.this,"Sukces",Toast.LENGTH_SHORT).show();
+
+                }else {
+
+                    Toast.makeText(MainActivity.this,"Error " + String.valueOf(billingResult.getDebugMessage()),Toast.LENGTH_SHORT).show();
+                }
+            }
+            @Override
+            public void onBillingServiceDisconnected() {
+                billingClient.startConnection(this);
+            }
+        });
+
+
+
+
         mAppBarConfiguration = new AppBarConfiguration.Builder(
                 R.id.nav_home, R.id.nav_rooms, R.id.nav_fragment_sun_energy_calculator_layout, R.id.nav_about_us)
                 .setDrawerLayout(drawer)
@@ -63,6 +125,9 @@ public class MainActivity extends AppCompatActivity {
         NavController navController = Navigation.findNavController(MainActivity.this, R.id.nav_host_fragment);
         NavigationUI.setupActionBarWithNavController(MainActivity.this, navController, mAppBarConfiguration);
         NavigationUI.setupWithNavController(navigationView, navController);
+
+
+
     }
 
 
@@ -86,9 +151,32 @@ public class MainActivity extends AppCompatActivity {
             startActivity(intent);
             Animatoo.animateSlideLeft(MainActivity.this);
             return true;
+        }else if(item.getItemId() == R.id.disable_ads){
+           startPurchase();
+
         }
         return super.onOptionsItemSelected(item);
     }
+
+    private void startPurchase() {
+        List<String> skuList = new ArrayList<>();
+        skuList.add("energy.cost.disable.ads");
+        SkuDetailsParams.Builder params = SkuDetailsParams.newBuilder();
+        params.setSkusList(skuList).setType(BillingClient.SkuType.INAPP);
+        billingClient.querySkuDetailsAsync(params.build(),
+                new SkuDetailsResponseListener() {
+                    @Override
+                    public void onSkuDetailsResponse(BillingResult billingResult,
+                                                     List<SkuDetails> skuDetailsList) {
+                        BillingFlowParams billingFlowParams = BillingFlowParams.newBuilder()
+                                .setSkuDetails(skuDetailsList.get(0))
+                                .build();
+                        billingClient.launchBillingFlow(MainActivity.this, billingFlowParams).getResponseCode();
+                        }
+                });
+    }
+
+
 
     private void copyMailtoCopyBoard(TextView studioMail) {
         ClipboardManager myClipboard;
@@ -99,4 +187,7 @@ public class MainActivity extends AppCompatActivity {
         Objects.requireNonNull(myClipboard).setPrimaryClip(myClip);
         Toast.makeText(this,getResources().getString(R.string.mail),Toast.LENGTH_SHORT).show();
     }
+
+
+
 }
